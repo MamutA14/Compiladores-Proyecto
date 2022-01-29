7edf6325d8f88b85d06f3f6a8b664363d6d193dd
Vladimir Sierra <vladimirsierra@ciencias.unam.mx>
2022-01-29 05:29:09 -0600

    Add pass 10: type-infer

--- src/middle.rkt
+++ src/middle.rkt
@@ -67,3 +67,144 @@
            [else `(const , 'Char , c) ])
         ])
    )
+
+
+
+
+;;================== PASS 10 : type-infer  ===============
+
+;;Definiciones auxiliares
+(define (unify t1 t2)
+    (if (and (type? t1) (type? t2))
+        (cond
+            [(equal? t1 t2) #t]
+            [(and (equal? 'List t1) (list? t2))  (equal? (car t2) 'List) ]
+            [(and (equal? 'List t2) (list? t1))  (equal? (car t1) 'List) ]
+            [(and (list? t1) (list? t2)) (and (unify (car t1) (car t2)))]
+            [else #f])
+        (error "Se esperaban 2 tipos")))
+
+
+;; Unas funciones auxiliares para realizar la busqueda de variables en el ambiente
+(define (find-type x env)
+    (cond
+        [(empty? env) (error 'find-type "Error, variable ~a no esta en el ambiente" x)]
+        [(eq? (caar env) x) (cadar env)] ;; caso cuando el ambiente tiene la forma '( (x t) ... ) , devolvemos t
+        [else (find-type x (cdr env))] ))  ;; llamada recursiva sobre el resto del ambiente si no coincide
+
+
+;; Funcion que dada una variable x, un tipo t y un ambiente env devuelve un nuevo ambiente
+;; resultado de agregar la tupla (x,t) al ambiente
+(define (add-var-to-env x t env)
+    (list (list x t) env))
+
+;; Funcion que verifica que dada una lista de tipos args, estos tipos sean los correctos para la operacion pr
+(define (check-args-types pr args )
+    (case pr
+        [(+ - * / < > equal? iszero? ++ --) (andmap (lambda (x) (equal? x 'Int)) args) ]  ;; Estas primitivas son sobre enteros
+        [(append cons)  (let ([tipo_e (car args)])  (equal? tipo_e (car (cddadr args) )) )]
+        [(concat) (let ([type (car args)])  (andmap (lambda (x) (equal? x type)) args) )]
+        [(elem?) (ormap (lambda (x) (and (list? x) (equal? (car x) 'List))) args) ]  ;; Estas operaciones van sobre listas
+        [(car cdr length equal-lst? empty?) (andmap (lambda (x) (and (list? x) (equal? (car x) 'List))) args) ] ))  ;; Estas operaciones van sobre listas
+
+
+(define global-env-J '())
+
+(define (J e env)
+    (nanopass-case (L6 Expr) e
+        [,x  (find-type x (append env global-env-J))]         ;; para variables buscamos directamente en el ambiente
+
+       [(define ,x ,e)
+            (let ([tipo (J e env)])
+            (begin
+            (set! global-env-J (add-var-to-env x tipo global-env-J))
+            tipo) )]
+
+        [(const ,t ,c ) t]              ;; para constantes tenemos el tipo especificado en el const
+
+        [(begin ,e* ... ,e)
+            (begin
+            (for ([i e*]) (J i env))
+            (J e env))]   ;; Retornamos el tipo de la ultima exprexion
+
+        [(primapp ,pr ,e* ...)
+            (let ([tipos  (map (lambda (x) (J x env)) e*)] )
+            (if (check-args-types pr tipos)
+                (case pr
+                    [(+ - * / ++ -- length) 'Int]
+                    [(< > equal? iszero? equal-lst? empty? elem?) 'Bool]
+                    [(concat) (car tipos)]
+                    [(cons append) (cadr tipos)]
+                    [(car) (caddr (car  (map (lambda (x) (J x env)) e*)))]
+                    [(cdr) (car  (map (lambda (x) (J x env)) e*) )])
+                (error 'J "Los tipos de ~a no son compatbles para la operacion ~a" e* pr)) )]
+
+        ;; Para el if verificamos que el tipo de e0 sea Bool, y los tipos de e1 y e2 sean los mismos
+        [(if ,e0 ,e1 ,e2)
+            (if (and (equal?  (J e0 env) 'Bool)  (unify (J e1 env) (J e2 env)))
+                (J e1 env)
+                (error 'J "El tipo de ~a debe ser Bool. Y el tipo de ~a debe ser igual al de ~a " e0 e1 e2) )]
+
+        ;; Para las lambdas el tipo es t -> type_of_body
+        [(lambda ([,x ,t]) ,body)  (list t '->  (J body (add-var-to-env x t env)))]
+
+        ;; para expresiones let devolvemos el tipo del body agregando (x t) al ambiente
+        [(let ([,x ,t ,e]) ,body)
+            (if  (unify t (J e env))
+                (J body (add-var-to-env x t env))
+                ;; hay un error si el tipo de e no coincide con t
+                (error 'J "El tipo de ~a no coincide con el de la expresion ~a." x e) )]
+
+        ;; para expresiones letrec devolvemos el tipo del body agregando (x t) al ambiente
+        [(letrec ([,x ,t ,e]) ,body)
+            (if  (unify t (J e (add-var-to-env x t env)))
+                (J body (add-var-to-env x t env))
+                ;; hay un error si el tipo de e no coincide con t
+                (error 'J "El tipo de ~a no coincide con el de la expresion ~a." x e) )]
+
+        ;; Para expresiones letrec devolvemos el tipo del body agregando (x t) al ambiente
+        [(letfun ([,x ,t ,e]) ,body)
+                (let ([tipo (J e env)])
+            ;; (if  (and (equal? '-> (cadr t)) (unify t (J e env)) )
+                (J body (add-var-to-env x tipo env)))]
+                ;; hay un error si el tipo de e no coincide con t
+               ;; (error 'J "El tipo de ~a no coincide con el de la expresion ~a." x e) )]
+
+        [(list ,e* ...)
+            ;; si es la lista vacia devoldemos List
+            (if (empty? e*)
+                'List
+                ;; Calculamos los tipos de los elementos
+                (let* ([types (map (lambda (x) (J x env)) e*) ]
+                        [t1 (car types)])
+                    ;; Si todos los mismos son los mismos deovlvemos List of T1
+                    (if (andmap (lambda (x) (unify x t1)) types)
+                        (list 'List 'of t1)
+                        (error 'J "Los elementos de la lista ~a no son del mismo tipo." e*)))  )]
+
+        [(,e0 ,e1)
+            (let ([t0 (J e0 env)] [t1 (J e1 env)])
+                (if (and (list? t0) (equal? (cadr t0) '->) (unify (car t0) t1))  ;; verificamos que el tipo de e0 sea T1->T2 y el de e1 sea T1
+                    (caddr t0)                                                   ;; Al aplicar la funcion se devuelve T2
+                    (error 'J "El tipo de ~a no es compatible con el de ~a  para realizar la aplicacion de funcion." e0 e1) )  )]
+        [(while [,[e0]] ,e1) (J e1 env)]
+        [(for [,x ,[e0]] ,e1)  (J e1 (add-var-to-env x (caddr e0) env)) ]
+  ))
+
+
+(define-pass type-infer : L6(ir) -> L6()
+    (Expr : Expr (ir) -> Expr ()
+        ;; Para let solo inferimos el tipo de t cuando de entrada es List, lo denombra a List of
+        [(let ([,x ,t ,[e]]) ,[body])
+            (case t
+                [(List) `(let ([,x ,(J e '()) ,e]) ,body) ]
+                [else   `(let ([,x ,t ,e]) ,body) ])]
+        ;; Para letrec solo inferimos el tipo de t cuando de entrada es Lambda o List, los cambia de T->T y List of respectivamente.
+        [(letrec ([,x ,t ,[e]]) ,[body])
+            (let ( [fixed-type (case t [(List Lambda) (J e '())] [else t]) ])
+                (begin  (displayln (~v t))
+                `(letrec ([,x ,fixed-type ,e]) ,body)
+            ))]
+        ;; Para letfun siempre inferimos el tipo de t, aplicando tanto para List como para Lambda
+        [(letfun ([,x ,t ,[e]]) ,[body])
+            `(letfun ([,x ,(J e '()) ,e]) ,body)]  ))
--- tests/middle-end-tests.mt
+++ tests/middle-end-tests.mt
@@ -3,3 +3,19 @@
 
 ;; para el pass 9
 (type-const (curry (front-passes (parse-LF '((lambda ([x Int] [y Int]) x ) (quot 1) ) ))))
+(type-const (curry (front-passes (parse-LF '(letrec ([f Lambda (lambda ([x Int])x )]) (f (quot 1)) ) ))))
+
+
+;; para el algoritmo J
+(J (type-const (curry (front-passes (parse-LF '(primapp cons (quot 1) (list (quot 1)) ) )))) '())
+(J (type-const (curry (front-passes (parse-LF '(for [x (list (quot 1)) ] x) )))) '())
+(J (type-const (curry (front-passes (parse-LF '(while [(quot 1)] (primapp + (quot 1) (quot 2) )) )))) '())
+
+
+
+;; para el pass 10
+(type-infer (type-const (curry (front-passes (parse-LF '(letrec ([f Lambda (lambda ([x Int])x )]) (f (quot 1)) ) )))))
+(type-infer (type-const (curry (front-passes (parse-LF '(let ([x List (list)]) x)) ))))
+(type-infer (type-const (curry (front-passes (parse-LF '(let ([x List (list (quot 1) (quot 2))]) x)) ))))
+
+
