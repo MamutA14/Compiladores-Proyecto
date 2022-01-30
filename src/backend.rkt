87d103dcadb48cd10f3c43dbd1636367544fd8c2
MamutA14 <alam@ciencias.unam.mx>
2022-01-29 21:17:17 -0600

    Se agrego el let en c

--- src/backend.rkt
+++ src/backend.rkt
@@ -285,6 +285,11 @@
                                (if (null? e*)
                                  ""
                                 (string-append (c-aux (first e*) tabla) ","(f (rest e*)))) )"};")]
+     [(let ,x ,body) ;; (let ((,x ,t ,e)) ,body* ... ,body)
+      (let ([ t (J (c-aux (hash-ref tabla x) tabla) "") ])
+                       (string-append t (string #\space) (symbol->string )))
+     (string-append 
+      (c-aux (hash-ref tabla x) tabla) (c-aux (hash-ref tabla x) tabla) "=" (c-aux body tabla))]
     [(,e0 ,e1) (string-append (c-aux e0 tabla)";\n"(c-aux e1 tabla)";")]
                                           ) )
 
